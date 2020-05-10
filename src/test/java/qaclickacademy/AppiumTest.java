cabc98768898437ce7e66d58deff8db45316a9ff
afoukal <ana.foukal@gmail.com>
2020-05-10 17:38:25 -0500

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAPpandroid()
+	{
+		System.out.println("NativeAPpandroid");
+	}
+	
+    @Test
+    public void IOSApps() {
+    
+    	
+    	System.out.println("IOSApps");
+    }
+
+
+}
